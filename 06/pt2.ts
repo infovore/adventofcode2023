34e2e7f28dd21211ad88ae17740630899cd6f21c
Tom Armitage <tom@infovore.org>
2023-12-06 13:32:52 +0000

    Working day two. Brute force, but still quick.

--- 06/pt2.ts
+++ 06/pt2.ts
@@ -8,17 +8,14 @@ if (process.argv.length < 3) {
 const filePath = process.argv[2]; // Assuming the file path is passed as the first command line argument
 const [timeString, distanceString] = fs.readFileSync(filePath).toString().split('\n');
 
-const times = timeString.split(/\s+/).filter(n => n.match(/\d+/)).map(n => parseInt(n, 10))
-const distances = distanceString.split(/\s+/).filter(n => n.match(/\d+/)).map(n => parseInt(n, 10))
+const times = [timeString.split("").filter(n => n.match(/\d/)).join("")].map(n => parseInt(n, 10))
+const distances = [distanceString.split("").filter(n => n.match(/\d/)).join("")].map(n => parseInt(n, 10))
 
 
 const output = times.map((timeLimit,i) => {
   const minDistance = distances[i];
-  // const results = [...Array.from(Array(timeLimit))]
-    // console.log(results)
   const results = [...Array.from(Array(timeLimit))].map((_,s) => {
     const t = timeLimit - s;
-    // console.log(t,s)
     return s*t;
   })
   return results.filter(r => r > minDistance).length
